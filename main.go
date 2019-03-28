093718f6fbf6b7f5d36eadf0ae20f0e34b6a8075
anthonydevelops <anthonydevelops@gmail.com>
2019-03-28 00:40:45 -0700

    Added dep and protobufs
    Signed-off-by: anthonydevelops <anthonydevelops@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
+// Backend API
+// Main handles the execution of handlers, generation, and connection to db
+
+//go:generate protoc --proto_path=model --proto_path=$GOPATH/src --gogo_out=model ./model/user.proto
+
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("TeamGen")
+	//Initialize router
+	r := mux.NewRouter()
+
+	//Route handlers
+	r.HandleFunc("/v1/api/user", getUser).Methods("GET")
 }
